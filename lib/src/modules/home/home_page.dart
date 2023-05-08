1eaabc9cb168c8c92f491227958471dc0b869d72
danielbrunobz <danielbrunobz@gmail.com>
2023-05-08 14:04:45 -0300

    settings

---
+++ lib/src/modules/home/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../../core/ui/helpers/loader.dart';
+import '../../core/ui/helpers/messages.dart';
+import '../../core/ui/helpers/size_extensions.dart';
+import '../../env/env.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with Loader, Messages {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          showLoader();
+          await Future.delayed(const Duration(seconds: 2));
+          hideLoader();
+          showError('Mensagem de erro');
+        },
+      ),
+      body: Container(
+        child: Container(
+          color: Colors.red,
+          width: context.percentWidth(.5),
+          height: context.percentHeight(.9),
+          child: Text(context.screenWidth.toString())),
+      ),
+    );
+  }
+}
