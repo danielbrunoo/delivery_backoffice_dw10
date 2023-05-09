b216a3da1da4777617f5cb248c19322e7732664b
danielbrunobz <danielbrunobz@gmail.com>
2023-05-09 15:26:41 -0300

    Template and MenuBar

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../core/ui/helpers/loader.dart';
 import '../../core/ui/helpers/messages.dart';
+import '../template/base_layout.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,44 +13,33 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> with Loader, Messages {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Home Page'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            showLoader();
-            await Future.delayed(const Duration(seconds: 2));
-            hideLoader();
-            showError('Mensagem de erro');
-          },
-        ),
-        body: Container(
-          child: Container(
-            // color: Colors.red,
-          
-            child: Column(
-              children: [
-                Padding(                  
-                  padding: const EdgeInsets.all(20.0),
-                  child: Form(
-                    autovalidateMode:  AutovalidateMode.onUserInteraction,
-                    child: TextFormField(
-                      decoration: const InputDecoration(label: Text('Login')),
-                      validator: (string) => 'Erro',
-                    ),
+    return BaseLayout(
+      body: Container(
+        child: Container(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Form(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  child: TextFormField(
+                    decoration: const InputDecoration(label: Text('Login')),
+                    validator: (string) => 'Erro',
                   ),
                 ),
-                SizedBox(
-                  width: 200,
-                  height: 50,
-                  child: ElevatedButton(onPressed: () {}, child: const Text('Botão'),
-                    ),
+              ),
+              SizedBox(
+                width: 200,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Botão'),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-        );
+      ),
+    );
   }
 }
