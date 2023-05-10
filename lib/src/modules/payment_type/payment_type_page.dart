290ef80d1f11c53343f9a00a202b378b03a1e577
danielbrunobz <danielbrunobz@gmail.com>
2023-05-10 12:10:14 -0300

    BaseHeader

--- lib/src/modules/payment_type/payment_type_page.dart
+++ lib/src/modules/payment_type/payment_type_page.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/payment_type_header.dart';
+
 class PaymentTypePage extends StatelessWidget {
   const PaymentTypePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Payment type'),
+    return Container(
+      color: Colors.grey[540],
+      padding: const EdgeInsets.only(left: 40, top: 10),
+      child: Column(
+        children: const [
+          PaymentTypeHeader(),
+        ],
+      ),
     );
   }
 }
---
+++ lib/src/modules/payment_type/widgets/payment_type_header.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/ui/widgets/base_header.dart';
+
+class PaymentTypeHeader extends StatelessWidget {
+  const PaymentTypeHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseHeader(
+        title: 'ADMINISTRAR FORMAS DE PAGAMENTO',
+        buttonLabel: 'ADICIONAR',
+        buttonPressed: () {},
+        );
+  }
+}
