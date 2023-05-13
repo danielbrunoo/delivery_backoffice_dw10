f3fb38eca664e32c5268d761c5aeb033082fbce3
danielbrunobz <danielbrunobz@gmail.com>
2023-05-13 15:17:52 -0300

    upload_html_helper

--- lib/src/modules/products/products_module.dart
+++ lib/src/modules/products/products_module.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'detail/product_detail_controller.dart';
 import 'detail/product_detail_page.dart';
 import 'home/products_controller.dart';
 import 'home/products_page.dart';
@@ -7,8 +8,13 @@ class ProductsModule extends Module {
 
    @override
    List<Bind> get binds => [
-    Bind.lazySingleton((i) => ProductsController(i(),),),
-   ];
+        Bind.lazySingleton(
+          (i) => ProductsController(i()),
+        ),
+        Bind.lazySingleton(
+          (i) => ProductDetailController(i()),
+        ),
+      ];
 
    @override
    List<ModularRoute> get routes => [
